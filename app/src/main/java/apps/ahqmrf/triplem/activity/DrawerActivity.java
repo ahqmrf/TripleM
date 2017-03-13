4afd191885d6356f91ec8de5e9bce13b7174e06a
ahqmrf <ahqmrf@gmail.com>
2017-03-13 14:13:21 +0600

    added an activity

---
+++ app/src/main/java/apps/ahqmrf/triplem/activity/DrawerActivity.java
@@ -0,0 +1,15 @@
+package apps.ahqmrf.triplem.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import apps.ahqmrf.triplem.R;
+
+public class DrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+    }
+}
